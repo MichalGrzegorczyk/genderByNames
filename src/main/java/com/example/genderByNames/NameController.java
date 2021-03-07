ce154a5cab09ca531e9fe200063520152aa78d6b
MichalGrzegorczyk <mgrzegorczykj@gmail.com>
2021-03-07 22:21:11 +0100

    Update NameController.java

--- src/main/java/com/example/genderByNames/NameController.java
+++ src/main/java/com/example/genderByNames/NameController.java
@@ -11,21 +11,60 @@ public class NameController {
 
 
 
-    @GetMapping("/gender")
-    public String gender(@RequestParam(value = "name", defaultValue = "Andrzej") String fullName) throws IOException {
+    @GetMapping("/genderVariant1")
+    public String genderVariant1(@RequestParam(value = "name", defaultValue = "Andrzej") String fullName) throws IOException {
 
-        String gender = "";
+        Name[] name = nameStringToArray(fullName);
+
+        String gender = name[0].gender();
+
+        return gender;
+    }
+
+    @GetMapping("/genderVariant2")
+    public String genderVariant2(@RequestParam(value = "name", defaultValue = "Andrzej") String fullName) throws IOException {
+
+        Name[] name = nameStringToArray(fullName);
+
+        String gender = dominantGender(name);
+
+                    return gender;
+        }
+
+    public Name[] nameStringToArray(String fullName){
 
         String[] namesArray = fullName.split(" ");
 
         Name[] name = new Name[namesArray.length];
 
-        for (int i = 0;i<namesArray.length;i++) {
+        for (int i = 0; i < namesArray.length; i++) {
             name[i] = new Name(namesArray[i]);
         }
-
-        gender = name[0].gender();
-
-        return  gender;
+        return name;
     }
+    public String dominantGender(Name[] name) throws IOException {
+
+        String gender = "";
+        int male = 0;
+        int female = 0;
+
+        for(int i = 0; i < name.length;i++){
+
+            if(name[i].isMale())
+                male++;
+
+            else if(name[i].isFemale())
+                female++;
+        }
+
+        if(male!=female){
+            if(male>female)
+                return "male";
+            else
+                return "female";
+        }
+
+        return "inconclusive";
+    }
+
 }
